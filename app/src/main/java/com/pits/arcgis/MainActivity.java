8e7595b739747bba1f5bad01f1d64d4fe5037e78
Omer Akram <om26er@gmail.com>
2016-01-25 05:48:37 -0800

    Minor changes, notthereyet

--- app/src/main/java/com/pits/arcgis/MainActivity.java
+++ app/src/main/java/com/pits/arcgis/MainActivity.java
@@ -8,6 +8,7 @@ import android.view.MenuItem;
 
 import com.esri.android.map.MapOptions;
 import com.esri.android.map.MapView;
+import com.esri.core.geometry.Point;
 import com.esri.core.map.Graphic;
 import com.esri.core.symbol.SimpleMarkerSymbol;
 import com.esri.core.tasks.na.NAFeaturesAsFeature;
@@ -21,16 +22,18 @@ public class MainActivity extends AppCompatActivity {
     private MapView mMapView;
 
     // The basemap switching menu items.
-    MenuItem mStreetsMenuItem;
-    MenuItem mTopoMenuItem;
-    MenuItem mGrayMenuItem;
-    MenuItem mOceansMenuItem;
+    private MenuItem mStreetsMenuItem;
+    private MenuItem mTopoMenuItem;
+    private MenuItem mGrayMenuItem;
+    private MenuItem mOceansMenuItem;
 
     // Create MapOptions for each type of basemap.
-    final MapOptions mTopoBasemap = new MapOptions(MapOptions.MapType.TOPO);
-    final MapOptions mStreetsBasemap = new MapOptions(MapOptions.MapType.STREETS);
-    final MapOptions mGrayBasemap = new MapOptions(MapOptions.MapType.GRAY);
-    final MapOptions mOceansBasemap = new MapOptions(MapOptions.MapType.OCEANS);
+    private final MapOptions mTopoBasemap = new MapOptions(MapOptions.MapType.TOPO);
+    private final MapOptions mStreetsBasemap = new MapOptions(MapOptions.MapType.STREETS);
+    private final MapOptions mGrayBasemap = new MapOptions(MapOptions.MapType.GRAY);
+    private final MapOptions mOceansBasemap = new MapOptions(MapOptions.MapType.OCEANS);
+    private final String routeTaskURL = "http://utility.arcgis.com/usrsvcs/servers/2ee8ad7509394" +
+            "799815c8825ba7112dd/rest/services/World/Route/NAServer/Route_World";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,29 +41,28 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         mMapView = (MapView) findViewById(R.id.map);
         mMapView.enableWrapAround(true);
-        final String routeTaskURL = "http://utility.arcgis.com/usrsvcs/servers/17d6ec37320149b49e6adf1045678e3a/rest/services/World/Route/NAServer/Route_World";
         new Thread(new Runnable() {
             @Override
             public void run() {
                 try {
                     RouteTask routeTask = RouteTask.createOnlineRouteTask(routeTaskURL, null);
                     RouteParameters routeParameters = routeTask.retrieveDefaultRouteTaskParameters();
-                    routeParameters.setImpedanceAttributeName("Length");
                     NAFeaturesAsFeature naFeatures = new NAFeaturesAsFeature();
                     SimpleMarkerSymbol sms = new SimpleMarkerSymbol(
                             Color.RED, 5, SimpleMarkerSymbol.STYLE.CIRCLE);
-                    com.esri.core.geometry.Point startPoint = new com.esri.core.geometry.Point();
-                    startPoint.setXY(48.859101, 2.292229);
-                    com.esri.core.geometry.Point stopPoint = new com.esri.core.geometry.Point();
-                    stopPoint.setXY(48.860589, 2.290870);
+
+                    Point startPoint = new Point(48.859101, 2.292229);
+                    Point stopPoint = new Point(48.860589, 2.290870);
 
                     Graphic graphic1 = new Graphic(startPoint, sms);
                     Graphic graphic2 = new Graphic(stopPoint, sms);
 
                     StopGraphic startPnt = new StopGraphic(graphic1);
                     StopGraphic stopPnt = new StopGraphic(graphic2);
+
                     naFeatures.setFeatures(new Graphic[] {startPnt, stopPnt});
                     routeParameters.setStops(naFeatures);
+
                     RouteResult mResults = routeTask.solve(routeParameters);
                     System.out.println(mResults.getStops());
                 } catch (Exception e) {
